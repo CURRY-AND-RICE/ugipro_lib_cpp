1f40ba3aa96f8c492e6482e3d687937cc7afc2d8
CURRY-AND-RICE <progoogram@gmail.com>
2024-04-28 18:37:31 +0900

    add zobrist hashing, coordinate compress

---
+++ ugilib/std_util/coord_compress.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+/**
+ * @file coord_compress.hpp
+ * @brief 座標圧縮の実装
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+namespace ugilib {
+    /**
+     * @brief 座標圧縮
+     * @tparam T 座標圧縮したい型
+     * @param v 圧縮したい配列
+     * @param descending trueの場合、降順に圧縮する
+     * @return unordered_map<T, ll> 元の値から圧縮した値へのマップ
+     * @note 圧縮後の値は0-indexed
+     * @note O(NlogN)
+    */
+    template <typename T>
+    unordered_map<T, int> compress(vector<T>& v, bool descending = false) {
+        // ユニークにする
+        vector<T> sorted_unique_v(v.begin(), v.end());  // コピー
+        auto end_unique = unique(sorted_unique_v.begin(), sorted_unique_v.end());  // ユニーク要素を前に詰める
+        sorted_unique_v.erase(end_unique, sorted_unique_v.end());  // 重複要素を削除
+        // ソート
+        if (descending) {
+            sort(sorted_unique_v.rbegin(), sorted_unique_v.rend());
+        } else {
+            sort(sorted_unique_v.begin(), sorted_unique_v.end());
+        }
+        // 圧縮
+        unordered_map<T, int> compressed;
+        rep(i, sorted_unique_v.size()) {
+            compressed[sorted_unique_v[i]] = i;
+        }
+        return compressed;
+    }
+} // namespace ugilib
--- ugilib/std_util/hashes.hpp
+++ ugilib/std_util/hashes.hpp
@@ -1,14 +1,43 @@
 #pragma once
+
+/**
+ * @file hashes.hpp
+ * @brief ハッシュ構造体の定義
+ * @note pair<T1, T2>, iterable<T>に対するハッシュ関数を定義
+*/
+
 #include <utility>
 using namespace std;
 
 namespace ugilib {
+    /**
+     * @brief pair<T1, T2>に対するハッシュ関数
+     * @tparam T1 pairの1つ目の要素の型
+     * @tparam T2 pairの2つ目の要素の型
+    */
     struct pair_hash {
         template <class T1, class T2>
         size_t operator () (const pair<T1, T2> &pair) const {
             auto hash1 = hash<T1>{}(pair.first);
             auto hash2 = hash<T2>{}(pair.second);
             return hash1 ^ (hash2 << 1);
+        }
+    };
+
+    /**
+     * @brief iterable<T>に対するハッシュ関数
+     * @tparam T iterableの要素の型
+     * @note zobrist hashing + DJB2 hashで実装
+     * @note 本来ならばzobrist hashingは乱数を用いるが、ここではstd::hashを用いている
+    */
+    struct zobrist_hash {
+        template <class T>
+        size_t operator () (const T &iterable) const {
+            size_t hash = 0;
+            for (const auto &item : iterable) {
+                hash ^= std::hash<typename T::value_type>{}(item) + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+            }
+            return hash;
         }
     };
 } // namespace ugilib
